e3d3785c4e0fa4a7fdceb19974d96df5c4f16cc0
Sibi Antony <sibi@gmail>
2014-01-09 00:19:01 +0100

    js, xul and css

---
+++ defaults/preferences/preferences.js
@@ -0,0 +1,6 @@
+// SSleuth notifier location. At the moment, either on urlbar or a toolbar button
+pref("extensions.ssleuth.notifier.location", 0); 
+// The panel fontsize. 0 = small, 1 = medium/large, 2 = larger
+pref("extensions.ssleuth.panel.fontsize", 0); 
+// Keyboard shortcut to bring up the panel. Not visible through preferences dialogue.
+pref("extensions.ssleuth.ui.keyshortcut", "control shift }"); 
