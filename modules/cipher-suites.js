e9f412b48ef42bfa95d949e5658e22c1f27f22d6
Sibi Antony <sibi.antony@gmail.com>
2014-02-13 18:24:35 +0100

    begin separate auth algorithm

--- modules/cipher-suites.js
+++ modules/cipher-suites.js
@@ -2,33 +2,42 @@ var EXPORTED_SYMBOLS = ["ssleuthCipherSuites"];
 
 const ssleuthCipherSuites = {
 	keyExchange : [
-		{ name: "TLS_ECDHE_RSA",	rank: 10,   pfs: 1, notes: "", },
-		{ name: "TLS_ECDHE_ECDSA",  rank: 10,   pfs: 1, notes: "", },
-		{ name: "TLS_ECDH_ECDSA",   rank: 9,	pfs: 0, notes: "", },
-		{ name: "TLS_ECDH_RSA",	 rank: 9,	pfs: 0, notes: "", },
-		{ name: "TLS_DHE_RSA",	  rank: 9,	pfs: 1, notes: "", },
-		{ name: "TLS_DHE_DSS",	  rank: 9,	pfs: 1, notes: "", },
-		{ name: "TLS_RSA",		  rank: 6,	pfs: 0, notes: "", },
-		{ name: "SSL_RSA",		  rank: 5,	pfs: 0, notes: "", },
-		{ name: "TLS_DHE_RSA_EXPORT",   rank: 5,pfs: 1, notes: "", },
-		{ name: "TLS_RSA_EXPORT",   rank: 4,	pfs: 0, notes: "", },
-		{ name: "TLS_EMPTY_RENEGOTIATION_INFO_SCSV",	rank: 0,	pfs: 0, notes: "" }
+		{ name: "TLS_ECDHE",	rank: 10,   pfs: 1, notes: "", },
+		{ name: "TLS_ECDH",	rank: 9,	pfs: 0, notes: "", },
+		{ name: "TLS_DHE",		rank: 9,	pfs: 1, notes: "", },
+		{ name: "TLS_RSA_WITH",			rank: 6,	pfs: 0, notes: "", },
+		{ name: "SSL_RSA_WITH",			rank: 5,	pfs: 0, notes: "", },
+		{ name: "TLS_RSA_EXPORT",	rank: 4,	pfs: 0, notes: "", },
+	],
+
+	authentication : [
+		{ name: "_RSA_", 			rank: 10, 	notes: "" },
+		{ name: "_DSA_", 			rank: 10, 	notes: "" },
+		{ name: "_ECDSA_", 		rank: 10, 	notes: "" },
+		/* Auth */
+		/* { name: "_DSS_", 		rank: 10, notes: "" } */
+
+	],
+
+	authKeyLength : [
+		{ 
+		}
 	],
 
 	bulkCipher : [
 		{ name: "AES_256_GCM",		rank: 10,   notes: "" },
-		{ name: "CAMELLIA_256_CBC",   rank: 10,   notes: "" },
+		{ name: "CAMELLIA_256_CBC", rank: 10,   notes: "" },
 		{ name: "AES_256_CBC",		rank: 10,   notes: "" },
-		{ name: "3DES_EDE_CBC",	   rank: 9,	notes: "" },
+		{ name: "3DES_EDE_CBC",		rank: 9,	notes: "" },
 		{ name: "AES_128_GCM",		rank: 8,	notes: "" },
 		{ name: "AES_128_CBC",		rank: 8,	notes: "" },
-		{ name: "CAMELLIA_128_CBC",   rank: 8,	notes: "" },
-		{ name: "SEED_CBC",		   rank: 8,	notes: "" },
+		{ name: "CAMELLIA_128_CBC", rank: 8,	notes: "" },
+		{ name: "SEED_CBC",			rank: 8,	notes: "" },
 		{ name: "RC4_128",			rank: 6,	notes: "RC4 considered unsafe. " },
 		{ name: "DES_CBC",			rank: 2,	notes: "Weak" },
-		{ name: "DES40_CBC",		  rank: 2,	notes: "Weak" },
-		{ name: "RC2_CBC_40",		 rank: 2,	notes: "Weak" },
-		{ name: "RC4_40",			 rank: 2,	notes: "Weak" },
+		{ name: "DES40_CBC",		rank: 2,	notes: "Weak" },
+		{ name: "RC2_CBC_40",		rank: 2,	notes: "Weak" },
+		{ name: "RC4_40",			rank: 2,	notes: "Weak" },
 	],
 
 	HMAC : [
