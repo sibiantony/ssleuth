6d93ef93966f7eebeac3c729c0e37a767116eb7f
Sibi Antony <sibi.antony@gmail.com>
2014-07-06 23:09:15 +0200

    more cosmetics..

--- modules/ssleuth-ui.js
+++ modules/ssleuth-ui.js
@@ -565,7 +565,7 @@ function showCertDetails(cert, domMismatch, ev) {
     .setAttribute("secure", cert.pubKeyMinSecure.toString()); 
 
   doc.getElementById("ssleuth-text-cert-sigalg")
-    .textContent = cert.signatureAlg.hmac + "/" + cert.signatureAlg.enc; 
+    .textContent = cert.signatureAlg.hmac + "/" + cert.signatureAlg.enc;
   rating = Number(cert.signatureAlg.rating * rp.signature/10).toFixed(1);
   doc.getElementById('ssleuth-cert-sigalg-rating')
     .textContent = rating + "/" + rp.signature; 
